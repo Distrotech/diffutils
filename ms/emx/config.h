49a8fa06517d91e798bf13c34bd446d55b1d401b
Paul Eggert <eggert@cs.ucla.edu>
1994-11-09 17:22:07 +0000

    Include <sys/emxload.h>.  Define _REGEX_RE_COMP.

--- ms/emx/config.h
+++ ms/emx/config.h
@@ -1,6 +1,7 @@
 /* emx configuration */
 
 #include <pc/config.h>
+#include <sys/emxload.h>
 
 #define initialize_main(pargc, pargv) \
   { \
@@ -11,5 +12,10 @@
   }
 
 #define pid_t int
+
+/* diffutils doesn't need re_comp,
+   but the shared library that diffutils builds
+   is used by other (old-fashioned) programs that need re_comp.  */
+#define _REGEX_RE_COMP
 
 #define STAT_BLOCKSIZE(s) 65536
