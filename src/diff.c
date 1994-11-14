893aeeb7e0234f11f9153b365f785fcadfc271d8
Paul Eggert <eggert@cs.ucla.edu>
1994-11-14 05:10:56 +0000

    (main): Invoke setlocale first thing, to tell library we're internationalized. -D FOO now outputs `/* ! FOO */ instead of `/* not FOO */'. (option_help_msgid): New constant. (usage): Use it, so message is translated one option at a time. (compare_files): For label, use file_label if set.

--- src/diff.c
+++ src/diff.c
@@ -253,6 +253,7 @@ main (argc, argv)
 
   /* Do our initializations.  */
   initialize_main (&argc, &argv);
+  setlocale (LC_ALL, "");
   program_name = argv[0];
   xmalloc_exit_failure = 2;
   output_style = OUTPUT_NORMAL;
@@ -315,7 +316,7 @@ main (argc, argv)
 		fatal ("context length specified twice");
 
 	      if (ck_atoi (optarg, &context))
-		fatal ("invalid context length argument");
+		fatal ("context length must be a nonnegative integer");
 	    }
 	  /* Fall through.  */
 	case 'c':
@@ -335,7 +336,7 @@ main (argc, argv)
 	  {
 	    int i, err = 0;
 	    static char const C_ifdef_group_formats[] =
-	      "#ifndef %s\n%%<#endif /* not %s */\n%c#ifdef %s\n%%>#endif /* %s */\n%c%%=%c#ifndef %s\n%%<#else /* %s */\n%%>#endif /* %s */\n";
+	      "#ifndef %s\n%%<#endif /* ! %s */\n%c#ifdef %s\n%%>#endif /* %s */\n%c%%=%c#ifndef %s\n%%<#else /* %s */\n%%>#endif /* %s */\n";
 	    char *b = xmalloc (sizeof (C_ifdef_group_formats)
 			       + 7 * strlen (optarg) - 14 /* 7*"%s" */
 			       - 8 /* 5*"%%" + 3*"%c" */);
@@ -349,7 +350,7 @@ main (argc, argv)
 		b += strlen (b) + 1;
 	      }
 	    if (err)
-	      error (0, 0, "conflicting #ifdef formats");
+	      error (0, 0, gettext ("conflicting #ifdef format"));
 	  }
 	  break;
 
@@ -485,7 +486,7 @@ main (argc, argv)
 	  break;
 
 	case 'v':
-	  printf ("diff - GNU diffutils version %s\n", version_string);
+	  printf ("diff - %s\n", version_string);
 	  exit (0);
 
 	case 'w':
@@ -533,7 +534,7 @@ main (argc, argv)
 	case 134:
 	  specify_style (OUTPUT_IFDEF);
 	  if (specify_format (&line_format[c - 132], optarg) != 0)
-	    error (0, 0, "conflicting line format");
+	    error (0, 0, gettext ("conflicting line format"));
 	  break;
 
 	case 135:
@@ -543,7 +544,7 @@ main (argc, argv)
 	    for (i = 0; i < sizeof (line_format) / sizeof (*line_format); i++)
 	      err |= specify_format (&line_format[i], optarg);
 	    if (err)
-	      error (0, 0, "conflicting line format");
+	      error (0, 0, gettext ("conflicting line format"));
 	  }
 	  break;
 
@@ -553,7 +554,7 @@ main (argc, argv)
 	case 139:
 	  specify_style (OUTPUT_IFDEF);
 	  if (specify_format (&group_format[c - 136], optarg) != 0)
-	    error (0, 0, "conflicting group format");
+	    error (0, 0, gettext ("conflicting group format"));
 	  break;
 
 	case 140:
@@ -720,84 +721,91 @@ summarize_regexp_list (reglist)
 }
 
 static void
-try_help (reason)
-     char const *reason;
+try_help (reason_msgid)
+     char const *reason_msgid;
 {
-  if (reason)
-    error (0, 0, "%s", reason);
-  error (2, 0, "Try `%s --help' for more information.", program_name);
+  if (reason_msgid)
+    error (0, 0, "%s", gettext (reason_msgid));
+  error (2, 0, gettext ("Try `%s --help' for more information."), program_name);
 }
 
 static void
 check_stdout ()
 {
   if (ferror (stdout) || fclose (stdout) != 0)
-    fatal ("write error");
+    fatal ("write failed");
 }
 
-static char const * const option_help[] = {
-"-i  --ignore-case  Consider upper- and lower-case to be the same.",
-"-w  --ignore-all-space  Ignore all white space.",
-"-b  --ignore-space-change  Ignore changes in the amount of white space.",
-"-B  --ignore-blank-lines  Ignore changes whose lines are all blank.",
-"-I RE  --ignore-matching-lines=RE  Ignore changes whose lines all match RE.",
+static char const * const option_help_msgid[] = {
+  "",
+  "-i  --ignore-case  Consider upper- and lower-case to be the same.",
+  "-w  --ignore-all-space  Ignore all white space.",
+  "-b  --ignore-space-change  Ignore changes in the amount of white space.",
+  "-B  --ignore-blank-lines  Ignore changes whose lines are all blank.",
+  "-I RE  --ignore-matching-lines=RE  Ignore changes whose lines all match RE.",
 #if HAVE_SETMODE
-"--binary  Read and write data in binary mode.",
+  "--binary  Read and write data in binary mode.",
 #endif
-"-a  --text  Treat all files as text.\n",
-"-c  -C NUM  --context[=NUM]  Output NUM (default 2) lines of copied context.",
-"-u  -U NUM  --unified[=NUM]  Output NUM (default 2) lines of unified context.",
-"  -NUM  Use NUM context lines.",
-"  -L LABEL  --label LABEL  Use LABEL instead of file name.",
-"  -p  --show-c-function  Show which C function each change is in.",
-"  -F RE  --show-function-line=RE  Show the most recent line matching RE.",
-"-q  --brief  Output only whether files differ.",
-"-e  --ed  Output an ed script.",
-"-n  --rcs  Output an RCS format diff.",
-"-y  --side-by-side  Output in two columns.",
-"  -w NUM  --width=NUM  Output at most NUM (default 130) characters per line.",
-"  --left-column  Output only the left column of common lines.",
-"  --suppress-common-lines  Do not output common lines.",
-"-DNAME  --ifdef=NAME  Output merged file to show `#ifdef NAME' diffs.",
-"--GTYPE-group-format=GFMT  Similar, but format GTYPE input groups with GFMT.",
-"--line-format=LFMT  Similar, but format all input lines with LFMT.",
-"--LTYPE-line-format=LFMT  Similar, but format LTYPE input lines with LFMT.",
-"  LTYPE is `old', `new', or `unchanged'.  GTYPE is LTYPE or `changed'.",
-"  GFMT may contain:",
-"    %<  lines from FILE1",
-"    %>  lines from FILE2",
-"    %=  lines common to FILE1 and FILE2",
-"    %[-][WIDTH][.[PREC]]{doxX}LETTER  printf-style spec for LETTER",
-"      LETTERs are as follows for new group, lower case for old group:",
-"        F  first line number",
-"        L  last line number",
-"        N  number of lines = L-F+1",
-"        E  F-1",
-"        M  L+1",
-"  LFMT may contain:",
-"    %L  contents of line",
-"    %l  contents of line, excluding any trailing newline",
-"    %[-][WIDTH][.[PREC]]{doxX}n  printf-style spec for input line number",
-"  Either GFMT or LFMT may contain:",
-"    %%  %",
-"    %c'C'  the single character C",
-"    %c'\\OOO'  the character with octal code OOO\n",
-"-l  --paginate  Pass the output through `pr' to paginate it.",
-"-t  --expand-tabs  Expand tabs to spaces in output.",
-"-T  --initial-tab  Make tabs line up by prepending a tab.\n",
-"-r  --recursive  Recursively compare any subdirectories found.",
-"-N  --new-file  Treat absent files as empty.",
-"-P  --unidirectional-new-file  Treat absent first files as empty.",
-"-s  --report-identical-files  Report when two files are the same.",
-"-x PAT  --exclude=PAT  Exclude files that match PAT.",
-"-X FILE  --exclude-from=FILE  Exclude files that match any pattern in FILE.",
-"-S FILE  --starting-file=FILE  Start with FILE when comparing directories.\n",
-"--horizon-lines=NUM  Keep NUM lines of the common prefix and suffix.",
-"-d  --minimal  Try hard to find a smaller set of changes.",
-"-H  --speed-large-files  Assume large files and many scattered small changes.\n",
-"-v  --version  Output version info.",
-"--help  Output this help.",
-0
+  "-a  --text  Treat all files as text.",
+  "",
+  "-c  -C NUM  --context[=NUM]  Output NUM (default 2) lines of copied context.",
+  "-u  -U NUM  --unified[=NUM]  Output NUM (default 2) lines of unified context.",
+  "  -NUM  Use NUM context lines.",
+  "  -L LABEL  --label LABEL  Use LABEL instead of file name.",
+  "  -p  --show-c-function  Show which C function each change is in.",
+  "  -F RE  --show-function-line=RE  Show the most recent line matching RE.",
+  "-q  --brief  Output only whether files differ.",
+  "-e  --ed  Output an ed script.",
+  "-n  --rcs  Output an RCS format diff.",
+  "-y  --side-by-side  Output in two columns.",
+  "  -w NUM  --width=NUM  Output at most NUM (default 130) characters per line.",
+  "  --left-column  Output only the left column of common lines.",
+  "  --suppress-common-lines  Do not output common lines.",
+  "-DNAME  --ifdef=NAME  Output merged file to show `#ifdef NAME' diffs.",
+  "--GTYPE-group-format=GFMT  Similar, but format GTYPE input groups with GFMT.",
+  "--line-format=LFMT  Similar, but format all input lines with LFMT.",
+  "--LTYPE-line-format=LFMT  Similar, but format LTYPE input lines with LFMT.",
+  "  LTYPE is `old', `new', or `unchanged'.  GTYPE is LTYPE or `changed'.",
+  "  GFMT may contain:",
+  "    %<  lines from FILE1",
+  "    %>  lines from FILE2",
+  "    %=  lines common to FILE1 and FILE2",
+  "    %[-][WIDTH][.[PREC]]{doxX}LETTER  printf-style spec for LETTER",
+  "      LETTERs are as follows for new group, lower case for old group:",
+  "        F  first line number",
+  "        L  last line number",
+  "        N  number of lines = L-F+1",
+  "        E  F-1",
+  "        M  L+1",
+  "  LFMT may contain:",
+  "    %L  contents of line",
+  "    %l  contents of line, excluding any trailing newline",
+  "    %[-][WIDTH][.[PREC]]{doxX}n  printf-style spec for input line number",
+  "  Either GFMT or LFMT may contain:",
+  "    %%  %",
+  "    %c'C'  the single character C",
+  "    %c'\\OOO'  the character with octal code OOO",
+  "",
+  "-l  --paginate  Pass the output through `pr' to paginate it.",
+  "-t  --expand-tabs  Expand tabs to spaces in output.",
+  "-T  --initial-tab  Make tabs line up by prepending a tab.",
+  "",
+  "-r  --recursive  Recursively compare any subdirectories found.",
+  "-N  --new-file  Treat absent files as empty.",
+  "-P  --unidirectional-new-file  Treat absent first files as empty.",
+  "-s  --report-identical-files  Report when two files are the same.",
+  "-x PAT  --exclude=PAT  Exclude files that match PAT.",
+  "-X FILE  --exclude-from=FILE  Exclude files that match any pattern in FILE.",
+  "-S FILE  --starting-file=FILE  Start with FILE when comparing directories.",
+  "",
+  "--horizon-lines=NUM  Keep NUM lines of the common prefix and suffix.",
+  "-d  --minimal  Try hard to find a smaller set of changes.",
+  "-H  --speed-large-files  Assume large files and many scattered small changes.",
+  "",
+  "-v  --version  Output version info.",
+  "--help  Output this help.",
+  "",
+  0
 };
 
 static void
@@ -805,10 +813,13 @@ usage ()
 {
   char const * const *p;
 
-  printf ("Usage: %s [OPTION]... FILE1 FILE2\n\n", program_name);
-  for (p = option_help;  *p;  p++)
-    printf ("  %s\n", *p);
-  printf ("\nIf FILE1 or FILE2 is `-', read standard input.\n");
+  printf (gettext ("Usage: %s [OPTION]... FILE1 FILE2\n"), program_name);
+  for (p = option_help_msgid;  *p;  p++)
+    if (**p)
+      printf ("  %s\n", gettext (*p));
+    else
+      putchar ('\n');
+  printf (gettext ("If a FILE is `-', read standard input.\n"));
 }
 
 static int
@@ -827,7 +838,7 @@ specify_style (style)
 {
   if (output_style != OUTPUT_NORMAL
       && output_style != style)
-    error (0, 0, "conflicting specifications of output style");
+    error (0, 0, gettext ("conflicting specifications of output style"));
   output_style = style;
 }
 
@@ -842,44 +853,44 @@ filetype (st)
   if (S_ISREG (st->st_mode))
     {
       if (st->st_size == 0)
-	return "regular empty file";
+	return gettext ("regular empty file");
       /* Posix.2 section 5.14.2 seems to suggest that we must read the file
 	 and guess whether it's C, Fortran, etc., but this is somewhat useless
 	 and doesn't reflect historical practice.  We're allowed to guess
 	 wrong, so we don't bother to read the file.  */
-      return "regular file";
+      return gettext ("regular file");
     }
-  if (S_ISDIR (st->st_mode)) return "directory";
+  if (S_ISDIR (st->st_mode)) return gettext ("directory");
 
   /* other Posix.1 file types */
 #ifdef S_ISBLK
-  if (S_ISBLK (st->st_mode)) return "block special file";
+  if (S_ISBLK (st->st_mode)) return gettext ("block special file");
 #endif
 #ifdef S_ISCHR
-  if (S_ISCHR (st->st_mode)) return "character special file";
+  if (S_ISCHR (st->st_mode)) return gettext ("character special file");
 #endif
 #ifdef S_ISFIFO
-  if (S_ISFIFO (st->st_mode)) return "fifo";
+  if (S_ISFIFO (st->st_mode)) return gettext ("fifo");
 #endif
 
   /* other Posix.1b file types */
 #ifdef S_TYPEISMQ
-  if (S_TYPEISMQ (st)) return "message queue";
+  if (S_TYPEISMQ (st)) return gettext ("message queue");
 #endif
 #ifdef S_TYPEISSEM
-  if (S_TYPEISSEM (st)) return "semaphore";
+  if (S_TYPEISSEM (st)) return gettext ("semaphore");
 #endif
 #ifdef S_TYPEISSHM
-  if (S_TYPEISSHM (st)) return "shared memory object";
+  if (S_TYPEISSHM (st)) return gettext ("shared memory object");
 #endif
 
   /* other popular file types */
   /* S_ISLNK is impossible with `fstat' and `stat'.  */
 #ifdef S_ISSOCK
-  if (S_ISSOCK (st->st_mode)) return "socket";
+  if (S_ISSOCK (st->st_mode)) return gettext ("socket");
 #endif
 
-  return "weird file";
+  return gettext ("weird file");
 }
 
 /* Compare two files (or dirs) with specified names
@@ -1003,7 +1014,7 @@ compare_files (dir0, name0, dir1, name1, depth)
 	= dir_file_pathname (dir, p ? p + 1 : fnm);
 
       if (strcmp (fnm, "-") == 0)
-	fatal ("can't compare - to a directory");
+	fatal ("cannot compare `-' to a directory");
 
       if (stat (filename, &inf[dir_arg].stat) != 0)
 	{
@@ -1081,8 +1092,10 @@ compare_files (dir0, name0, dir1, name1, depth)
 
 	  /* See Posix.2 section 4.17.6.1.1 for this format.  */
 	  message5 ("File %s is a %s while file %s is a %s\n",
-		    inf[0].name, filetype (&inf[0].stat),
-		    inf[1].name, filetype (&inf[1].stat));
+		    file_label[0] ? file_label[0] : inf[0].name,
+		    filetype (&inf[0].stat),
+		    file_label[1] ? file_label[1] : inf[1].name,
+		    filetype (&inf[1].stat));
 
 	  /* This is a difference.  */
 	  val = 1;
@@ -1093,7 +1106,9 @@ compare_files (dir0, name0, dir1, name1, depth)
 	   && (inf[0].desc == -1 || S_ISREG (inf[0].stat.st_mode))
 	   && (inf[1].desc == -1 || S_ISREG (inf[1].stat.st_mode)))
     {
-      message ("Files %s and %s differ\n", inf[0].name, inf[1].name);
+      message ("Files %s and %s differ\n",
+	       file_label[0] ? file_label[0] : inf[0].name,
+	       file_label[1] ? file_label[1] : inf[1].name);
       val = 1;
     }
   else
@@ -1150,7 +1165,8 @@ compare_files (dir0, name0, dir1, name1, depth)
     {
       if (print_file_same_flag)
 	message ("Files %s and %s are identical\n",
-		 inf[0].name, inf[1].name);
+		 file_label[0] ? file_label[0] : inf[0].name,
+		 file_label[1] ? file_label[1] : inf[1].name);
     }
   else
     fflush (stdout);
