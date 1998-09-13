1a50d285b938342ab331b10f7f05bd22f6502982
Paul Eggert <eggert@cs.ucla.edu>
1998-09-13 05:33:07 +0000

    Fix spacing.

--- src/context.c
+++ src/context.c
@@ -1,21 +1,22 @@
 /* Context-format output routines for GNU DIFF.
-   Copyright (C) 1988,1989,1991,1992,1993,1994 Free Software Foundation, Inc.
+   Copyright 1988, 89,91,92,93,94,95, 1997 Free Software Foundation, Inc.
 
-This file is part of GNU DIFF.
+   This file is part of GNU DIFF.
 
-GNU DIFF is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+   GNU DIFF is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-GNU DIFF is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   GNU DIFF is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GNU DIFF; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation, 
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include "diff.h"
 
@@ -24,7 +25,7 @@ static struct change *find_hunk PARAMS((struct change *));
 static void mark_ignorable PARAMS((struct change *));
 static void pr_context_hunk PARAMS((struct change *));
 static void pr_unidiff_hunk PARAMS((struct change *));
-static void print_context_label PARAMS ((char const *, struct file_data *, char const *));
+static void print_context_label PARAMS((char const *, struct file_data *, char const *));
 static void print_context_number_range PARAMS((struct file_data const *, int, int));
 static void print_unidiff_number_range PARAMS((struct file_data const *, int, int));
 
