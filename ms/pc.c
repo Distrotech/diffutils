7a3d2137801b5848c56cc36fa151393342b0faf8
Paul Eggert <eggert@cs.ucla.edu>
1998-09-14 00:34:04 +0000

    Update FSF address. (quote_system_arg): Renamed from system_quote_arg.

--- ms/pc.c
+++ ms/pc.c
@@ -1,21 +1,22 @@
 /* Functions specific to PC operating systems
-   Copyright (C) 1994 Free Software Foundation, Inc.
+   Copyright 1994, 1995, 1998 Free Software Foundation, Inc.
 
-This file is part of GNU DIFF.
+   This file is part of GNU DIFF.
 
-GNU DIFF is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+   GNU DIFF is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-GNU DIFF is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   GNU DIFF is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GNU DIFF; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation, 
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include <string.h>
 
@@ -36,7 +37,7 @@ filename_lastdirchar (char const *filename)
    If QUOTED is null, return the length without any side effects.  */
 
 size_t
-system_quote_arg (char *quoted, char const *arg)
+quote_system_arg (char *quoted, char const *arg)
 {
   int needs_quoting = !*arg || strchr (arg, ' ') || strchr (arg, '\t');
   size_t backslashes = 0;
