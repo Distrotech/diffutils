6e9a130189c23ea1f75fd1cfe52be084e9d6957f
Paul Eggert <eggert@cs.ucla.edu>
1998-09-14 00:26:54 +0000

    Initial revision

---
+++ lib/freesoft.c
@@ -0,0 +1,9 @@
+/* Free software message ID.  */
+
+#include "diff.h"
+
+char const free_software_msgid[] = "\
+This program comes with NO WARRANTY, to the extent permitted by law.\n\
+You may redistribute copies of this program\n\
+under the terms of the GNU General Public License.\n\
+For more information about these matters, see the file named COPYING.";
