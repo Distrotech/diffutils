1c2ed8ea5b7561400ef64ce60c71181d5a1f4649
Paul Eggert <eggert@cs.ucla.edu>
2001-11-24 20:08:03 +0000

    Assume C89 or better. int -> bool for booleans. Include error.h, exclude.h, xalloc.h.
    (dir_sort): Return 0 on error, 1 on success.  All callers changed.
    compare_names -> compare_names_for_qsort.
    
    (compare_names): Try strcasecmp if ignore_file_name_case.  Then try
    strcoll.  Use file_name_cmp only as a last resort.  Warn about
    strcasecmp or strcoll failure.
    (compare_names_for_qsort): New function.
    
    (diff_dirs): Use compare_names rather than filename_cmp.

--- src/dir.c
+++ src/dir.c
@@ -1,5 +1,7 @@
 /* Read, sort and compare two directories.  Used for GNU DIFF.
-   Copyright 1988, 89, 92, 93, 94, 95, 1998 Free Software Foundation, Inc.
+
+   Copyright (C) 1988, 1989, 1992, 1993, 1994, 1995, 1998, 2001 Free
+   Software Foundation, Inc.
 
    This file is part of GNU DIFF.
 
@@ -15,10 +17,13 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; see the file COPYING.
-   If not, write to the Free Software Foundation, 
+   If not, write to the Free Software Foundation,
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include "diff.h"
+#include <error.h>
+#include <exclude.h>
+#include <xalloc.h>
 
 /* Read the directory named by DIR and store into DIRDATA a sorted vector
    of filenames for its contents.  DIR->desc == -1 means this directory is
@@ -31,17 +36,14 @@ struct dirdata
   char *data;	/* Allocated storage for file names.  */
 };
 
-static int compare_names PARAMS((void const *, void const *));
-static int dir_loop PARAMS((struct comparison const *, int));
-static int dir_sort PARAMS((struct file_data const *, struct dirdata *));
+static bool dir_loop (struct comparison const *, int);
+static int compare_names_for_qsort (void const *, void const *);
 
-static int
-dir_sort (dir, dirdata)
-     struct file_data const *dir;
-     struct dirdata *dirdata;
+static bool
+dir_sort (struct file_data const *dir, struct dirdata *dirdata)
 {
   register struct dirent *next;
-  register int i;
+  register size_t i;
 
   /* Address of block containing the files that are described.  */
   char const **names;
@@ -63,11 +65,11 @@ dir_sort (dir, dirdata)
       /* Open the directory and check for errors.  */
       register DIR *reading = opendir (dir->name);
       if (!reading)
-	return -1;
+	return 0;
 
       /* Initialize the table of filenames.  */
 
-      data_alloc = max (1, (size_t) dir->stat.st_size);
+      data_alloc = MAX (1, (size_t) dir->stat.st_size);
       data_used = 0;
       dirdata->data = data = xmalloc (data_alloc);
 
@@ -79,12 +81,12 @@ dir_sort (dir, dirdata)
 	  char *d_name = next->d_name;
 	  size_t d_size = NAMLEN (next) + 1;
 
-	  /* Ignore the files `.' and `..' */
+	  /* Ignore "." and "..".  */
 	  if (d_name[0] == '.'
 	      && (d_name[1] == 0 || (d_name[1] == '.' && d_name[2] == 0)))
 	    continue;
 
-	  if (excluded_filename (d_name))
+	  if (excluded_filename (excluded, d_name))
 	    continue;
 
 	  while (data_alloc < data_used + d_size)
@@ -98,19 +100,18 @@ dir_sort (dir, dirdata)
 	  int e = errno;
 	  closedir (reading);
 	  errno = e;
-	  return -1;
+	  return 0;
 	}
 #if CLOSEDIR_VOID
       closedir (reading);
 #else
       if (closedir (reading) != 0)
-	return -1;
+	return 0;
 #endif
     }
 
   /* Create the `names' table from the `data' table.  */
-  dirdata->names = names = (char const **) xmalloc (sizeof (char *)
-						    * (nnames + 1));
+  dirdata->names = names = xmalloc ((nnames + 1) * sizeof *names);
   for (i = 0;  i < nnames;  i++)
     {
       names[i] = data;
@@ -119,19 +120,49 @@ dir_sort (dir, dirdata)
   names[nnames] = 0;
 
   /* Sort the table.  */
-  qsort (names, nnames, sizeof (char *), compare_names);
+  qsort (names, nnames, sizeof (char *), compare_names_for_qsort);
 
-  return 0;
+  return 1;
 }
 
-/* Sort the files now in the table.  */
+/* Compare file names, returning a value compatible with strcmp.  */
 
 static int
-compare_names (file1, file2)
-     void const *file1, *file2;
+compare_names (char const *name1, char const *name2)
 {
-  return filename_cmp (* (char const *const *) file1,
-		       * (char const *const *) file2);
+  int r = 0;
+  int e = 0;
+
+  if (ignore_file_name_case)
+    {
+      errno = 0;
+      r = strcasecmp (name1, name2);
+      e = errno;
+    }
+
+  if (! r)
+    {
+      errno = 0;
+      r = strcoll (name1, name2);
+      if (errno)
+	e = errno;
+    }
+
+  if (e)
+    error (0, e, _("Warning: cannot compare file name `%s' to `%s'"),
+	   name1, name2);
+
+  return r ? r : file_name_cmp (name1, name2);
+}
+
+/* A wrapper for compare_names suitable as an argument for qsort.  */
+
+static int
+compare_names_for_qsort (void const *file1, void const *file2)
+{
+  char const *const *f1 = file1;
+  char const *const *f2 = file2;
+  return compare_names (*f1, *f2);
 }
 
 /* Compare the contents of two directories named in CMP.
@@ -152,9 +183,9 @@ compare_names (file1, file2)
    or 2 if trouble is encountered in opening files.  */
 
 int
-diff_dirs (cmp, handle_file)
-     struct comparison const *cmp;
-     int (*handle_file) PARAMS((struct comparison const *, char const *, char const *));
+diff_dirs (struct comparison const *cmp,
+	   int (*handle_file) (struct comparison const *,
+			       char const *, char const *))
 {
   struct dirdata dirdata[2];
   int val = 0;			/* Return value.  */
@@ -170,7 +201,7 @@ diff_dirs (cmp, handle_file)
 
   /* Get sorted contents of both dirs.  */
   for (i = 0; i < 2; i++)
-    if (dir_sort (&cmp->file[i], &dirdata[i]) != 0)
+    if (! dir_sort (&cmp->file[i], &dirdata[i]))
       {
 	perror_with_name (cmp->file[i].name);
 	val = 2;
@@ -184,11 +215,11 @@ diff_dirs (cmp, handle_file)
       /* If `-S name' was given, and this is the topmost level of comparison,
 	 ignore all file names less than the specified starting name.  */
 
-      if (dir_start_file && ! cmp->parent)
+      if (starting_file && ! cmp->parent)
 	{
-	  while (*names0 && filename_cmp (*names0, dir_start_file) < 0)
+	  while (*names0 && compare_names (*names0, starting_file) < 0)
 	    names0++;
-	  while (*names1 && filename_cmp (*names1, dir_start_file) < 0)
+	  while (*names1 && compare_names (*names1, starting_file) < 0)
 	    names1++;
 	}
 
@@ -199,7 +230,7 @@ diff_dirs (cmp, handle_file)
 	     At the end of a dir,
 	     pretend the "next name" in that dir is very large.  */
 	  int nameorder = (!*names0 ? 1 : !*names1 ? -1
-			   : filename_cmp (*names0, *names1));
+			   : compare_names (*names0, *names1));
 	  int v1 = (*handle_file) (cmp,
 				   0 < nameorder ? 0 : *names0++,
 				   nameorder < 0 ? 0 : *names1++);
@@ -221,10 +252,8 @@ diff_dirs (cmp, handle_file)
 
 /* Return nonzero if CMP is looping recursively in argument I.  */
 
-static int
-dir_loop (cmp, i)
-     struct comparison const *cmp;
-     int i;
+static bool
+dir_loop (struct comparison const *cmp, int i)
 {
   struct comparison const *p = cmp;
   while ((p = p->parent))
