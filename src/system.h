b6092f7a19c7b78a5c002a3fd80d0b8b61c3ea61
Paul Eggert <eggert@cs.ucla.edu>
2001-11-25 00:21:28 +0000

    Assume C89 or better. (_GNU_SOURCE): Remove; config.h now defines it. (alloca): Declare like coreutils does it. (verify, TYPE_SIGNED, TYPE_MINIMUM, TYPE_MAXIMUM, O_RDWR, S_IRUSR, S_IWUSR): New macros. (STAT_BLOCKSIZE): Parenthesize definiens. <inttypes.h>: Include if HAVE_INTTYPES_H. (CHAR_MAX, INT_MAX): Remove. (PTRDIFF_MAX, SIZE_MAX): New macros. (strtoumax): New decl. Include stddef.h. (bzero): Remove. (bindtextdomain, textdomain, N_): New macros. (ISPRINT, ISSPACE): Remove ifndef wrappers. (ISUPPER, ISDIGIT): Remove. (TOLOWER): New macro. (MIN): Renamed from min; all callers changed. (MAX): Likewise, from max. (lin): New type. (LIN_MAX): New macro. (file_name_cmp): Renamed from filename_cmp.  All callers changed. (file_name_lastdirchar): Renamed from file_name_lastdirchar. All callers changed. (could_be_mvfs_stat_bug, could_be_nfs_stat_bug, dev_may_have_duplicate_ino): Remove. (HAVE_SETMODE, NULL_DEVICE): New macros. (same_file): Do not check attributes. (same_file_attributes): New macro.

--- src/system.h
+++ src/system.h
@@ -1,5 +1,7 @@
 /* System dependent declarations.
-   Copyright 1988, 89, 92, 93, 94, 95, 1998 Free Software Foundation, Inc.
+
+   Copyright (C) 1988, 1989, 1992, 1993, 1994, 1995, 1998, 2001 Free
+   Software Foundation, Inc.
 
    This file is part of GNU DIFF.
 
@@ -18,31 +20,67 @@
    If not, write to the Free Software Foundation,
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#define _GNU_SOURCE
+#include <config.h>
 
-/* We must define `__attribute__', `volatile' and `const' first
-   so that they're used consistently in all system includes.
-   (config.h defines `const').  */
-#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 6)
+/* Don't bother to support K&R C compilers any more; it's not worth
+   the trouble.  These macros prevent some library modules from being
+   compiled in K&R C mode.  */
+#define PARAMS(Args) Args
+#define PROTOTYPES 1
+
+/* Define `__attribute__' and `volatile' first
+   so that they're used consistently in all system includes.  */
+#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 6) || __STRICT_ANSI__
 # define __attribute__(x)
 #endif
-#if !__STDC__
-# ifndef volatile
+#if defined const && !defined volatile
 # define volatile
+#endif
+
+/* Handle `alloca' now, since the AIX pragma must occur before any
+   system include files.  */
+#if ! HAVE_C_VARARRAYS
+# if HAVE_ALLOCA_H
+#  include <alloca.h>
+# else
+#  ifdef _AIX
+ #  pragma alloca
+#  else
+#   ifdef _WIN32
+#    include <malloc.h>
+#    include <io.h>
+#   else
+#    ifndef alloca
+void *alloca (size_t);
+#    endif
+#   endif
+#  endif
 # endif
 #endif
-#include <config.h>
+
+/* Verify a requirement at compile-time (unlike assert, which is runtime).  */
+#define verify(name, assertion) struct name { char a[(assertion) ? 1 : -1]; }
+
+
+/* Determine whether an integer type is signed, and its bounds.
+   This code assumes two's (or one's!) complement with no holes.  */
+
+/* The extra casts work around common compiler bugs,
+   e.g. Cray C 5.0.3.0 when t == time_t.  */
+#ifndef TYPE_SIGNED
+# define TYPE_SIGNED(t) (! ((t) 0 < (t) -1))
+#endif
+#ifndef TYPE_MINIMUM
+# define TYPE_MINIMUM(t) ((t) (TYPE_SIGNED (t) \
+			       ? ~ (t) 0 << (sizeof (t) * CHAR_BIT - 1) \
+			       : (t) 0))
+#endif
+#ifndef TYPE_MAXIMUM
+# define TYPE_MAXIMUM(t) ((t) (~ (t) 0 - TYPE_MINIMUM (t)))
+#endif
 
 #include <sys/types.h>
 #include <sys/stat.h>
-
-#if __STDC__
-# define PARAMS(args) args
-# define VOID void
-#else
-# define PARAMS(args) ()
-# define VOID char
-#endif
 
 #if STAT_MACROS_BROKEN
 # undef S_ISBLK
@@ -53,22 +91,22 @@
 # undef S_ISSOCK
 #endif
 #ifndef S_ISDIR
-#define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
+# define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
 #endif
 #ifndef S_ISREG
-#define S_ISREG(mode) (((mode) & S_IFMT) == S_IFREG)
+# define S_ISREG(mode) (((mode) & S_IFMT) == S_IFREG)
 #endif
-#if !defined (S_ISBLK) && defined (S_IFBLK)
-#define S_ISBLK(mode) (((mode) & S_IFMT) == S_IFBLK)
+#if !defined S_ISBLK && defined S_IFBLK
+# define S_ISBLK(mode) (((mode) & S_IFMT) == S_IFBLK)
 #endif
-#if !defined (S_ISCHR) && defined (S_IFCHR)
-#define S_ISCHR(mode) (((mode) & S_IFMT) == S_IFCHR)
+#if !defined S_ISCHR && defined S_IFCHR
+# define S_ISCHR(mode) (((mode) & S_IFMT) == S_IFCHR)
 #endif
-#if !defined (S_ISFIFO) && defined (S_IFFIFO)
-#define S_ISFIFO(mode) (((mode) & S_IFMT) == S_IFFIFO)
+#if !defined S_ISFIFO && defined S_IFFIFO
+# define S_ISFIFO(mode) (((mode) & S_IFMT) == S_IFFIFO)
 #endif
-#if !defined (S_ISSOCK) && defined (S_IFSOCK)
-#define S_ISSOCK(mode) (((mode) & S_IFMT) == S_IFSOCK)
+#if !defined S_ISSOCK && defined S_IFSOCK
+# define S_ISSOCK(mode) (((mode) & S_IFMT) == S_IFSOCK)
 #endif
 
 #if HAVE_UNISTD_H
@@ -76,20 +114,20 @@
 #endif
 
 #ifndef SEEK_SET
-#define SEEK_SET 0
+# define SEEK_SET 0
 #endif
 #ifndef SEEK_CUR
-#define SEEK_CUR 1
+# define SEEK_CUR 1
 #endif
 
 #ifndef STDIN_FILENO
-#define STDIN_FILENO 0
+# define STDIN_FILENO 0
 #endif
 #ifndef STDOUT_FILENO
-#define STDOUT_FILENO 1
+# define STDOUT_FILENO 1
 #endif
 #ifndef STDERR_FILENO
-#define STDERR_FILENO 2
+# define STDERR_FILENO 2
 #endif
 
 #if HAVE_TIME_H
@@ -107,26 +145,35 @@
 #endif
 
 #if !HAVE_DUP2
-# define dup2(f,t)	(close (t),  fcntl (f,F_DUPFD,t))
+# define dup2(f, t) (close (t), fcntl (f, F_DUPFD, t))
 #endif
 
 #ifndef O_RDONLY
-#define O_RDONLY 0
+# define O_RDONLY 0
+#endif
+#ifndef O_RDWR
+# define O_RDWR 2
+#endif
+#ifndef S_IRUSR
+# define S_IRUSR 0400
+#endif
+#ifndef S_IWUSR
+# define S_IWUSR 0200
 #endif
 
 #if HAVE_SYS_WAIT_H
 # include <sys/wait.h>
 #endif
 #ifndef WEXITSTATUS
-#define WEXITSTATUS(stat_val) ((unsigned) (stat_val) >> 8)
+# define WEXITSTATUS(stat_val) ((unsigned) (stat_val) >> 8)
 #endif
 #ifndef WIFEXITED
-#define WIFEXITED(stat_val) (((stat_val) & 255) == 0)
+# define WIFEXITED(stat_val) (((stat_val) & 255) == 0)
 #endif
 
 #ifndef STAT_BLOCKSIZE
 # if HAVE_ST_BLKSIZE
-#  define STAT_BLOCKSIZE(s) (s).st_blksize
+#  define STAT_BLOCKSIZE(s) ((s).st_blksize)
 # else
 #  define STAT_BLOCKSIZE(s) (8 * 1024)
 # endif
@@ -161,24 +208,25 @@
 # endif
 #endif
 
-#if HAVE_LIMITS_H
-# include <limits.h>
+#include <limits.h>
+
+#if HAVE_INTTYPES_H
+# include <inttypes.h>
 #endif
-#ifndef CHAR_MAX
-#define CHAR_MAX ('\377' < 0 ? '\177' : '\377')
+#ifndef PTRDIFF_MAX
+# define PTRDIFF_MAX TYPE_MAXIMUM (ptrdiff_t)
 #endif
-#ifndef INT_MAX
-#define INT_MAX 2147483647
+#ifndef SIZE_MAX
+# define SIZE_MAX TYPE_MAXIMUM (size_t)
 #endif
-#ifndef CHAR_BIT
-#define CHAR_BIT 8
+#if ! HAVE_STRTOUMAX  && ! defined strtoumax
+uintmax_t strtoumax (char const *, char **, int);
 #endif
+
+#include <stddef.h>
 
 #if STDC_HEADERS || HAVE_STRING_H
 # include <string.h>
-# ifndef bzero
-#  define bzero(s, n) memset (s, 0, n)
-# endif
 #else
 # if !HAVE_STRCHR
 #  define strchr index
@@ -202,11 +250,15 @@ void *memchr ();
 # include <libintl.h>
 #else
 # define gettext(msgid) (msgid)
+# define bindtextdomain(domain, dir)
+# define textdomain(domain)
 #endif
 
 #define _(msgid) gettext (msgid)
+#define N_(msgid) (msgid)
 
 #include <ctype.h>
+
 /* CTYPE_DOMAIN (C) is nonzero if the unsigned char C can safely be given
    as an argument to <ctype.h> macros like `isspace'.  */
 #if STDC_HEADERS
@@ -214,86 +266,89 @@ void *memchr ();
 #else
 # define CTYPE_DOMAIN(c) ((unsigned) (c) <= 0177)
 #endif
-#ifndef ISPRINT
 #define ISPRINT(c) (CTYPE_DOMAIN (c) && isprint (c))
-#endif
-#ifndef ISSPACE
 #define ISSPACE(c) (CTYPE_DOMAIN (c) && isspace (c))
-#endif
-#ifndef ISUPPER
-#define ISUPPER(c) (CTYPE_DOMAIN (c) && isupper (c))
+
+#if STDC_HEADERS
+# define TOLOWER(c) tolower (c)
+#else
+# ifndef _tolower
+#  define _tolower(c) tolower (c)
+# endif
+# define TOLOWER(c) (CTYPE_DOMAIN (c) && isupper (c) ? _tolower (c) : (c))
 #endif
 
-#ifndef ISDIGIT
+/* ISDIGIT differs from isdigit, as follows:
+   - Its arg may be any int or unsigned int; it need not be an unsigned char.
+   - It's guaranteed to evaluate its argument exactly once.
+   - It's typically faster.
+   POSIX 1003.2-1992 section 2.5.2.1 page 50 lines 1556-1558 says that
+   only '0' through '9' are digits.  Prefer ISDIGIT to isdigit unless
+   it's important to use the locale's definition of `digit' even when the
+   host does not conform to POSIX.  */
 #define ISDIGIT(c) ((unsigned) (c) - '0' <= 9)
-#endif
-
-#ifndef _tolower
-#define _tolower(c) tolower (c)
-#endif
 
 #include <errno.h>
 #if !STDC_HEADERS
  extern int errno;
 #endif
 
-#ifdef min
-#undef min
+#undef MIN
+#undef MAX
+#define MIN(a, b) ((a) <= (b) ? (a) : (b))
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
+
+#if HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# define bool unsigned char
 #endif
-#ifdef max
-#undef max
-#endif
-#define min(a,b) ((a) <= (b) ? (a) : (b))
-#define max(a,b) ((a) >= (b) ? (a) : (b))
 
-/* This section contains Posix-compliant defaults for macros
+/* Type used for fast comparison of several bytes at a time.  */
+
+#ifndef word
+# define word uintmax_t
+#endif
+
+/* The integer type of a line number.  Since files are read into main
+   memory, ptrdiff_t should be wide enough.  */
+
+typedef ptrdiff_t lin;
+#define LIN_MAX PTRDIFF_MAX
+verify (lin_is_signed, TYPE_SIGNED (lin));
+verify (lin_is_wide_enough, sizeof (ptrdiff_t) <= sizeof (lin));
+verify (lin_is_printable_as_long, sizeof (lin) <= sizeof (long));
+
+/* This section contains POSIX-compliant defaults for macros
    that are meant to be overridden by hand in config.h as needed.  */
 
-#ifndef filename_cmp
-#define filename_cmp(a, b) strcmp (a, b)
+#ifndef file_name_cmp
+# define file_name_cmp strcmp
 #endif
 
-#ifndef filename_lastdirchar
-#define filename_lastdirchar(filename) strrchr (filename, '/')
+#ifndef file_name_lastdirchar
+# define file_name_lastdirchar(name) strrchr (name, '/')
 #endif
 
 #ifndef HAVE_FORK
-#define HAVE_FORK 1
+# define HAVE_FORK 1
+#endif
+
+#ifndef HAVE_SETMODE
+# define HAVE_SETMODE 0
 #endif
 
 #ifndef initialize_main
-#define initialize_main(argcp, argvp)
+# define initialize_main(argcp, argvp)
 #endif
 
-#if WITH_MVFS_STAT_BUG
-# if ! HAVE_ST_FSTYPE_STRING
-#  define could_be_mvfs_stat_bug(s) 1
-# else
-#  define could_be_mvfs_stat_bug(s) (strcmp ((s)->st_fstype, "mvfs") == 0)
-# endif
-#else
-# define could_be_mvfs_stat_bug(s) 0
-#endif
-
-#if WITH_NFS_STAT_BUG
-# if ! HAVE_ST_FSTYPE_STRING
-#  define could_be_nfs_stat_bug(s) 1
-# else
-#  define could_be_nfs_stat_bug(s) (strcmp ((s)->st_fstype, "nfs") == 0)
-# endif
-#else
-# define could_be_nfs_stat_bug(s) 0
-#endif
-
-/* Might S's device have two distinct files with the same st_ino?  */
-#ifndef dev_may_have_duplicate_ino
-#define dev_may_have_duplicate_ino(s) (could_be_mvfs_stat_bug (s) \
-				       || could_be_nfs_stat_bug (s))
+#ifndef NULL_DEVICE
+# define NULL_DEVICE "/dev/null"
 #endif
 
 /* Do struct stat *S, *T describe the same special file?  */
 #ifndef same_special_file
-# if HAVE_ST_RDEV && defined (S_ISBLK) && defined (S_ISCHR)
+# if HAVE_ST_RDEV && defined S_ISBLK && defined S_ISCHR
 #   define same_special_file(s, t) \
       (((S_ISBLK ((s)->st_mode) && S_ISBLK ((t)->st_mode)) \
 	|| (S_ISCHR ((s)->st_mode) && S_ISCHR ((t)->st_mode))) \
@@ -305,20 +360,43 @@ void *memchr ();
 
 /* Do struct stat *S, *T describe the same file?  Answer -1 if unknown.  */
 #ifndef same_file
-#define same_file(s, t) \
-  (same_special_file (s, t) \
-   ? 1 \
-   : ((s)->st_ino ^ (t)->st_ino) | ((s)->st_dev ^ (t)->st_dev) \
-   ? 0 \
-   : ! dev_may_have_duplicate_ino (s) \
-   ? 1 \
-   : (((s)->st_mode ^ (t)->st_mode) \
-      | ((s)->st_nlink ^ (t)->st_nlink) \
-      | ((s)->st_uid ^ (t)->st_uid) \
-      | ((s)->st_gid ^ (t)->st_gid) \
-      | ((s)->st_size ^ (t)->st_size) \
-      | ((s)->st_mtime ^ (t)->st_mtime) \
-      | ((s)->st_ctime ^ (t)->st_ctime)) \
-   ? 0 \
-   : -1)
+# define same_file(s, t) \
+    ((((s)->st_ino == (t)->st_ino) && ((s)->st_dev == (t)->st_dev)) \
+     || same_special_file (s, t))
+#endif
+
+/* Do struct stat *S, *T have the same file attributes?
+
+   POSIX says that two files are identical if st_ino and st_dev are
+   the same, but many filesystems incorrectly assign the same (device,
+   inode) pair to two distinct files, including:
+
+   - GNU/Linux NFS servers that export all local filesystems as a
+     single NFS filesystem, if a local device number (st_dev) exceeds
+     255, or if a local inode number (st_ino) exceeds 16777215.
+
+   - Network Appliance NFS servers in snapshot directories; see
+     Network Appliance bug #195.
+
+   - ClearCase MVFS; see bug id ATRia04618.
+
+   Check whether two files that purport to be the same have the same
+   attributes, to work around instances of this common bug.  Do not
+   inspect all attributes, only attributes useful in checking for this
+   bug.
+
+   It's possible for two distinct files on a buggy filesystem to have
+   the same attributes, but it's not worth slowing down all
+   implementations (or complicating the configuration) to cater to
+   these rare cases in buggy implementations.  */
+
+#ifndef same_file_attributes
+# define same_file_attributes(s, t) \
+   ((s)->st_mode == (t)->st_mode \
+    && (s)->st_nlink == (t)->st_nlink \
+    && (s)->st_uid == (t)->st_uid \
+    && (s)->st_gid == (t)->st_gid \
+    && (s)->st_size == (t)->st_size \
+    && (s)->st_mtime == (t)->st_mtime \
+    && (s)->st_ctime == (t)->st_ctime)
 #endif
