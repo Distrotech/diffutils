c33d179ab620a88209cad033c8d6cdd6f6d09f82
Paul Eggert <eggert@cs.ucla.edu>
2001-12-03 13:39:54 +0000

    (usage): Clarify.

--- src/cmp.c
+++ src/cmp.c
@@ -157,10 +157,16 @@ usage (void)
 
   printf (_("Usage: %s [OPTION]... FILE1 [FILE2 [SKIP1 [SKIP2]]]\n"),
 	  program_name);
-  printf (_("If a FILE is `-' or missing, read standard input.\n"));
-  printf (_("SKIP1 and SKIP2 are the number of bytes to skip in each file.\n"));
+  printf ("\n");
   for (p = option_help_msgid;  *p;  p++)
     printf ("  %s\n", _(*p));
+  printf ("\n");
+  printf (_("If a FILE is `-' or missing, read standard input.\n"));
+  printf (_("SKIP1 and SKIP2 are the number of bytes to skip in each file.\n"));
+  printf (_("SKIP values may be followed by the following multiplicative suffixes:\n"));
+  printf (_("kB 1000, K 1024, MB 1,000,000, M 1,048,576,\n"));
+  printf (_("GB 1,000,000,000, G 1,073,741,824, and so on for T, P, E, Z, Y.\n"));
+  printf ("\n");
   printf (_("Report bugs to <bug-gnu-utils@gnu.org>.\n"));
 }
 
