36671926115bb6cd29876470afbd85cdda33b443
Jim Meyering <meyering@fb.com>
2013-03-24 11:09:13 -0700

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -29,7 +29,7 @@ bootstrap-tools = autoconf,automake,gnulib
 # Now that we have better tests, make this the default.
 export VERBOSE = yes
 
-old_NEWS_hash = 7d70c18e90495732e671b3c94da01651
+old_NEWS_hash = 1d1b7468264850b366b1138bd78e5520
 
 # Tell maint.mk's syntax-check rules that diff gets config.h directly or
 # via diff.h or system.h.
